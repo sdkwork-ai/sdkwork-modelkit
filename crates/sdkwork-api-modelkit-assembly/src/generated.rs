1ac52ab4304ac0cc2fe5eba0797b326254eb90f7
sdkwork-ai <sdkwork@zowalk.com>
2026-08-30 20:31:12 +0800

    chore(config): sync environment / topology configuration

--- crates/sdkwork-api-modelkit-assembly/src/generated.rs
+++ crates/sdkwork-api-modelkit-assembly/src/generated.rs
@@ -3,4 +3,6 @@
 pub const ROUTE_CRATE_COUNT: usize = 1;
 
 #[allow(dead_code)]
-pub const ROUTE_CRATE_PACKAGES: &[&str] = &["sdkwork-routes-modelkit-app-api"];
+pub const ROUTE_CRATE_PACKAGES: &[&str] = &[
+    "sdkwork-routes-modelkit-app-api",
+];
